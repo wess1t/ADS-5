07b02b7acd02ff1149c3cd87425f305112b61dd4
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-05 18:58:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,13 +24,14 @@ bool isOperator(char c) {
 std::string infx2pstfx(const std::string& inf) {
   TStack<char, 100> operators;
   std::string result;
-  
+
   for (size_t i = 0; i < inf.length(); i++) {
     char c = inf[i];
-    
-    if (c == ' ')
+
+    if (c == ' ') {
       continue;
-      
+    }
+
     if (isdigit(c)) {
       while (i < inf.length() && isdigit(inf[i])) {
         result += inf[i];
@@ -38,11 +39,9 @@ std::string infx2pstfx(const std::string& inf) {
       }
       result += ' ';
       i--;
-    }
-    else if (c == '(') {
+    } else if (c == '(') {
       operators.push(c);
-    }
-    else if (c == ')') {
+    } else if (c == ')') {
       while (!operators.empty() && operators.top() != '(') {
         result += operators.pop();
         result += ' ';
@@ -50,9 +49,8 @@ std::string infx2pstfx(const std::string& inf) {
       if (!operators.empty() && operators.top() == '(') {
         operators.pop();
       }
-    }
-    else if (isOperator(c)) {
-      while (!operators.empty() && operators.top() != '(' && 
+    } else if (isOperator(c)) {
+      while (!operators.empty() && operators.top() != '(' &&
              getPriority(operators.top()) >= getPriority(c)) {
         result += operators.pop();
         result += ' ';
@@ -60,27 +58,29 @@ std::string infx2pstfx(const std::string& inf) {
       operators.push(c);
     }
   }
-  
+
   while (!operators.empty()) {
     result += operators.pop();
     result += ' ';
   }
 
-  if (!result.empty() && result.back() == ' ')
+  if (!result.empty() && result.back() == ' ') {
     result.pop_back();
-  
+  }
+
   return result;
 }
 
 int eval(const std::string& post) {
   TStack<int, 100> operands;
-  
+
   for (size_t i = 0; i < post.length(); i++) {
     char c = post[i];
-    
-    if (c == ' ')
+
+    if (c == ' ') {
       continue;
-    
+    }
+
     if (isdigit(c)) {
       int number = 0;
       while (i < post.length() && isdigit(post[i])) {
@@ -89,13 +89,12 @@ int eval(const std::string& post) {
       }
       operands.push(number);
       i--;
-    }
-    else if (isOperator(c)) {
+    } else if (isOperator(c)) {
       if (operands.getSize() >= 2) {
         int b = operands.pop();
         int a = operands.pop();
         int result = 0;
-        
+
         switch (c) {
           case '+':
             result = a + b;
@@ -114,6 +113,6 @@ int eval(const std::string& post) {
       }
     }
   }
-  
+
   return operands.pop();
 }
