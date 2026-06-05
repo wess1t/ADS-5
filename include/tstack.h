ab495a57222ab1dd294ddad921f126831982b16f
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-05 14:59:56 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T data[size];
+  int top_i;
+
+public:
+  TStack() : top_i(-1) {}
+  void push(const T& item) {
+    if (top_i < size - 1)
+      data[++top_i] = item;
+  }
+  T pop() {
+    if (top_i >= 0)
+      return data[top_i--];
+    return T();
+  }
+  T top() const {
+    if (top_i >= 0)
+      return data[top_i];
+    return T();
+  }
+  bool empty() const {
+    return top_i == -1;
+  }
+  int size() const {
+    return top_i + 1;
+  }
+  void clear() {
+    top_i = -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
