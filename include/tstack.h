655e29cab21186433f2c49f3a6259793d11456d5
Рыбина Екатерина <rybinae58@gmail.com>
2026-06-05 18:55:10 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,37 +2,45 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int N>
 class TStack {
-
-private:
-  T data[size];
-  int top_i;
-
-public:
-  TStack() : top_i(-1) {}
+ private:
+  T data[N];
+  int top_index;
+  
+ public:
+  TStack() : top_index(-1) {}
+  
   void push(const T& item) {
-    if (top_i < size - 1)
-      data[++top_i] = item;
+    if (top_index < N - 1) {
+      data[++top_index] = item;
+    }
   }
+  
   T pop() {
-    if (top_i >= 0)
-      return data[top_i--];
+    if (top_index >= 0) {
+      return data[top_index--];
+    }
     return T();
   }
+  
   T top() const {
-    if (top_i >= 0)
-      return data[top_i];
+    if (top_index >= 0) {
+      return data[top_index];
+    }
     return T();
   }
+  
   bool empty() const {
-    return top_i == -1;
+    return top_index == -1;
   }
-  int size() const {
-    return top_i + 1;
+  
+  int getSize() const {
+    return top_index + 1;
   }
+  
   void clear() {
-    top_i = -1;
+    top_index = -1;
   }
 };
 
